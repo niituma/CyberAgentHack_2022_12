c8b300d9f7296eaea94a08ff4beb822a981af5c5
kmamonnassi <ka82573@jk2.so-net.ne.jp>
2022-12-16 13:20:14 +0900

    プレイヤーのアニメーションを制御するプログラムのコメントアウトを追加

--- Assets/Scripts/Player/IPlayerAnimator.cs
+++ Assets/Scripts/Player/IPlayerAnimator.cs
@@ -1,9 +1,12 @@
 namespace CleanCity
 {
-    public interface IPlayerAnimator 
+    public interface IPlayerAnimator
     {
+        /// <summary>移動開始のアニメーション</summary>
         void StartMove();
+        /// <summary>移動終了のアニメーション</summary>
         void EndMove();
+        /// <summary>ダメージを受けた時のアニメーション</summary>
         void Damage();
     }
 }
--- Assets/Scripts/Player/PlayerAnimator.cs
+++ Assets/Scripts/Player/PlayerAnimator.cs
@@ -6,16 +6,19 @@ namespace CleanCity
     {
         [SerializeField] private Animator animator;
 
+        /// <summary>移動開始のアニメーション</summary>
         public void StartMove()
         {
             animator.SetBool("Move", true);
         }
 
+        /// <summary>移動終了のアニメーション</summary>
         public void EndMove()
         {
             animator.SetBool("Move", false);
         }
 
+        /// <summary>ダメージを受けた時のアニメーション</summary>
         public void Damage()
         {
             animator.SetTrigger("Damage");
