9250cc85b9c8da48c79ed0d5d52d82875438e050
na <kanapodream@gmail.com>
2022-12-16 18:17:35 +0900

    ゲームの状態が変わると作用するプログラムを作成

---
+++ Assets/Scripts/UI/RetryButton.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using UnityEngine.EventSystems;
+
+namespace CleanCity.UI
+{
+  public class RetryButton : MonoBehaviour,IPointerClickHandler
+  {
+    public void OnPointerClick(PointerEventData eventData)
+    {
+      GameSystem.RestartGame();
+    }
+  }
+}
---
+++ Assets/Scripts/Utility/DestroyOnStatusChange.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using CleanCity;
+using UnityEngine;
+
+namespace Utility
+{
+  public class DestroyOnStatusChange : MonoBehaviour
+  {
+
+    [SerializeField] private GameSystem.State state;
+    
+    private void Start()
+    {
+      GameSystem.Singleton.onStatusChanged += Destroy;
+    }
+
+    private void Destroy(GameSystem.State _, GameSystem.State to)
+    {
+      if (to == state)
+      {
+        Destroy(gameObject);
+        GameSystem.Singleton.onStatusChanged -= Destroy;
+      }
+    }
+  }
+}
