c26bac63be06570cde90e3ffce07e7b59f12428d
kmamonnassi <ka82573@jk2.so-net.ne.jp>
2022-12-16 14:05:21 +0900

    文字コード修正

--- Assets/Scripts/Enemy/EnemyMove.cs
+++ Assets/Scripts/Enemy/EnemyMove.cs
@@ -1,20 +1,28 @@
-using UnityEngine;
+﻿using UnityEngine;
 
 namespace CleanCity
 {
 	public class EnemyMove : MonoBehaviour, ISetEnemyMoveDir
 	{
 		[SerializeField] private float speed = 100;
+		
 		private Rigidbody rb;
+		[SerializeField] private Vector3 moveDir;
 
 		private void Start()
 		{
 			rb = GetComponent<Rigidbody>();
 		}
 
+		/// <summary>移動方向設定</summary>
 		public void SetMoveDir(Vector3 dir)
 		{
-			rb.velocity = dir * speed;
+			moveDir = dir;
+		}
+
+		private void Update()
+		{
+			rb.velocity = moveDir * speed;
 		}
 	}
 }
--- Assets/Scripts/Enemy/ISetEnemyMoveDir.cs
+++ Assets/Scripts/Enemy/ISetEnemyMoveDir.cs
@@ -1,9 +1,10 @@
-using UnityEngine;
+﻿using UnityEngine;
 
 namespace CleanCity
 {
     public interface ISetEnemyMoveDir
     {
+        /// <summary>移動方向設定</summary>
         void SetMoveDir(Vector3 dir);
     }
 }
