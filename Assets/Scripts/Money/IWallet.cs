869cc973624d2d1ba8ce4ad05babd1bc4121f3bb
kmamonnassi <ka82573@jk2.so-net.ne.jp>
2022-12-18 02:47:53 +0900

    プレイヤーがゴミをゴミ箱に入れた時、お金がたまるように

--- Assets/Scripts/Money/IWallet.cs
+++ Assets/Scripts/Money/IWallet.cs
@@ -6,7 +6,9 @@ namespace CleanCity
 	{
 		int GetMoney { get; }
 		event Action<(int nowMoney, int addMoney)> OnChangeMoney;
+		/// <summary>Ç®ã‡í«â¡</summary>
 		void AddMoney(int money);
+		/// <summary>Ç®ã‡å∏è≠</summary>
 		void ReduceMoney(int money);
 	}
 }
