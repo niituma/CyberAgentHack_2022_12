224f7ca9da9d8891704a1e9644a63f1d72c0cb11
raitonakatani <kbc21b27@stu.kawahara.ac.jp>
2022-12-17 23:25:43 +0900

    BGMを追加

---
+++ Assets/Scripts/Sound/SoundBank.cs
@@ -0,0 +1,81 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+namespace CleanCity
+{
+    //SEを再生する関数を書くclass
+    public class SoundBank : MonoBehaviour
+    {
+        // 音データの再生装置を格納する
+        private new AudioSource audio;
+
+        // 音データを格納する
+  //      [SerializeField] private AudioClip step;      //足音
+        [SerializeField] private AudioClip button;      //ボタン
+        [SerializeField] private AudioClip pickup;      //拾う
+        [SerializeField] private AudioClip discard;     //捨てる
+        [SerializeField] private AudioClip conflict;    //衝突
+        [SerializeField] private AudioClip title;      //ボタン
+        [SerializeField] private AudioClip ingame;      //拾う
+        [SerializeField] private AudioClip result;     //捨てる
+
+        private void Start()
+        {
+            // コンポーネントから再生装置を検出する
+            audio = gameObject.AddComponent<AudioSource>();
+
+            GameSystem.Singleton.onStatusChanged += async (before, to) =>
+            {
+                if (to == GameSystem.State.InGame)
+                {
+                    //音を鳴らす
+                    audio.PlayOneShot(ingame);
+                }
+            };
+        }
+
+        //// 足音のSE
+        //public void StepSE()
+        //{
+        //    //音を鳴らす
+        //    audio.PlayOneShot(step);
+        //}
+
+        // ボタンを押したときに再生するSE
+        public void IsInPutButton()
+        {
+            //音を鳴らす
+            audio.PlayOneShot(button);
+        }
+
+        // ゴミを拾った時に再生するSE
+        public void TrashPickUP()
+        {
+            //音を鳴らす
+            audio.PlayOneShot(pickup);
+        }
+
+        // ゴミを捨てた時に再生するSE
+        public void TrashDiscard()
+        {
+            //音を鳴らす
+            audio.PlayOneShot(discard);
+        }
+
+        // プレイヤーが衝突した時に再生するSE
+        public void IsPlayerConflict()
+        {
+            //音を鳴らす
+            audio.PlayOneShot(conflict);
+        }
+
+        // リザルトの時につかうSE（使わない可能性もある）
+        public void ResultSE()
+        {
+            //音を鳴らす
+            audio.PlayOneShot(result);
+        }
+    }
+}
