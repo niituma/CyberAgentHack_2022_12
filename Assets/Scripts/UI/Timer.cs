15a85cdeecc4292fab4186bfb506790ab940e144
na <kanapodream@gmail.com>
2022-12-18 12:27:39 +0900

    残り10秒以下でタイマの文字の色を変えるようにした

--- Assets/Scripts/UI/Timer.cs
+++ Assets/Scripts/UI/Timer.cs
@@ -18,6 +18,8 @@ public class Timer : MonoBehaviour
     [SerializeField]
     TextMeshProUGUI _timeText;
 
+    private IWaveSystem _waveSystem;
+
     private void Awake()
     {
         Locator<Timer>.Register(this);
@@ -30,12 +32,22 @@ public class Timer : MonoBehaviour
         {
             _countdownSeconds = _countdownMinutes * 60;
         };
+        _waveSystem = Locator<IWaveSystem>.Resolve();
     }
 
     void Update()
     {
-        if (GameSystem.Singleton?.Status != GameSystem.State.InGame 
-            || GameSystem.Singleton?.Status == GameSystem.State.Result) { return; }
+        if (GameSystem.Singleton.Status != GameSystem.State.InGame
+            || GameSystem.Singleton.Status == GameSystem.State.Result)
+        {
+            return;
+        }
+
+        if (_waveSystem.IsBreakTime)
+        {
+            _timeText.color = Color.black;
+            return;
+        }
 
         if (_countdownSeconds <= 0) 
         { 
@@ -46,5 +58,14 @@ public class Timer : MonoBehaviour
         _countdownSeconds -= Time.deltaTime;
         var span = new TimeSpan(0, 0, (int)_countdownSeconds);
         _timeText.text = span.ToString(@"mm\:ss");
+
+        if (_countdownSeconds <= 10)
+        {
+            _timeText.color = Color.red;
+        }
+        else
+        {
+            _timeText.color = Color.black;
+        }
     }
 }
