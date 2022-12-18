3c0d5f09a6d7ff0dffdc561747741dad7d546696
na <kanapodream@gmail.com>
2022-12-18 12:01:23 +0900

    休憩時間はタイマーが止まるように

--- Assets/Scripts/UI/Timer.cs
+++ Assets/Scripts/UI/Timer.cs
@@ -18,6 +18,8 @@ public class Timer : MonoBehaviour
     [SerializeField]
     TextMeshProUGUI _timeText;
 
+    private IWaveSystem _waveSystem;
+
     private void Awake()
     {
         Locator<Timer>.Register(this);
@@ -30,12 +32,21 @@ public class Timer : MonoBehaviour
         {
             _countdownSeconds = _countdownMinutes * 60;
         };
+        _waveSystem = Locator<IWaveSystem>.Resolve();
     }
 
     void Update()
     {
-        if (GameSystem.Singleton?.Status != GameSystem.State.InGame 
-            || GameSystem.Singleton?.Status == GameSystem.State.Result) { return; }
+        if (GameSystem.Singleton.Status != GameSystem.State.InGame
+            || GameSystem.Singleton.Status == GameSystem.State.Result)
+        {
+            return;
+        }
+
+        if (_waveSystem.IsBreakTime)
+        {
+            return;
+        }
 
         if (_countdownSeconds <= 0) 
         { 
