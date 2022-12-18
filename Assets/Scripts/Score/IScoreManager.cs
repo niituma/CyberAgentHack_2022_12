715d41d2ed3442f30d2e48c1818a5472520b7204
na <kanapodream@gmail.com>
2022-12-18 11:24:21 +0900

    ScoreManagerで今までに拾ったごみの数を管理しないようにした

--- Assets/Scripts/Score/IScoreManager.cs
+++ Assets/Scripts/Score/IScoreManager.cs
@@ -6,12 +6,10 @@ namespace CleanCity
     {
         /// <summary>スコア取得</summary>
         int GetScore { get; }
-        /// <summary>処理したごみの数取得</summary>
-        int GetClearedGarbage { get; }
         /// <summary>スコアが追加されたときのコールバック</summary>
         event Action<(int nowScore, int addScore)> OnAddScore;
         /// <summary>処理したごみの数が追加されたときのコールバック</summary>
-        event Action<int> OnAddClearedGarbage;
+        event Action OnAddClearedGarbage;
         /// <summary>スコアを追加する</summary>
         void AddScore(int score);
         /// <summary>処理したごみの数を追加</summary>
--- Assets/Scripts/Score/ScoreManager.cs
+++ Assets/Scripts/Score/ScoreManager.cs
@@ -1,23 +1,17 @@
 ﻿using System;
-using CleanCity.UI;
-using Utility;
 
 namespace CleanCity
 {
     public class ScoreManager : IScoreManager
     {
         private int score;
-        private int clearedGarbage;
 
 		/// <summary>スコア取得</summary>
 		public int GetScore => score;
 
-		/// <summary>処理したごみの数取得</summary>
-		public int GetClearedGarbage => clearedGarbage;
-
 		/// <summary>スコアが追加されたときのコールバック</summary>
 		public event Action<(int nowScore, int addScore)> OnAddScore;
-		public event Action<int> OnAddClearedGarbage;
+		public event Action OnAddClearedGarbage;
 
 		/// <summary>スコアを追加する</summary>
 		public void AddScore(int score)
@@ -29,8 +23,7 @@ namespace CleanCity
 		/// <summary>処理したごみの数を追加</summary>
 		public void AddClearedGarbage()
 		{
-			clearedGarbage++;
-			OnAddClearedGarbage?.Invoke(clearedGarbage);
+			OnAddClearedGarbage?.Invoke();
 		}
 	}
 }
