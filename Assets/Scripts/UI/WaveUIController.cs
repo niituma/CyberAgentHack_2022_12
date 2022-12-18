c5288d538ea44c102bcc237396dd88f0d26f8a21
SubNiitsuma <sakezuma@gmail.com>
2022-12-18 12:26:14 +0900

    breakTimeをUI表示できるようにした。

--- Assets/Scripts/UI/WaveUIController.cs
+++ Assets/Scripts/UI/WaveUIController.cs
@@ -11,15 +11,27 @@ public class WaveUIController : MonoBehaviour
     [SerializeField]
     TextMeshProUGUI _waveText;
     CanvasGroup _canvasGroup;
+    RectTransform rectTransform;
+
+    Tween BreakTimeAnim;
     // Start is called before the first frame update
     void Start()
     {
         _canvasGroup = GetComponent<CanvasGroup>();
+        rectTransform = GetComponent<RectTransform>();
         _canvasGroup.alpha = 0f;
 
         Locator<IWaveSystem>.Resolve().OnAddWave += (num) =>
         {
             SetAnimUI(num);
+        };
+
+        Locator<IWaveSystem>.Resolve().OnBreakTimeStatusChanged += (active) =>
+        {
+            if (active)
+            {
+                SetBreakTimeUI();
+            }
         };
 
         if (GameSystem.Singleton)
@@ -41,10 +53,24 @@ public class WaveUIController : MonoBehaviour
 
     Tween SetWaveUI(int num)
     {
+        if (BreakTimeAnim != null) { BreakTimeAnim.Kill(); }
         _waveText.text = $"WAVE:{num}";
         transform.localScale = Vector3.zero;
         GetComponent<CanvasGroup>().alpha = 0;
         return DOTween.Sequence().Append(transform.DOScale(1f, 1f).SetEase(Ease.InQuad))
                                  .Join(_canvasGroup.DOFade(1, 1.5f));
+    }
+
+    void SetBreakTimeUI()
+    {
+        _waveText.text = $"BREAK TIME";
+        transform.localScale = Vector3.one;
+        GetComponent<CanvasGroup>().alpha = 1;
+        BreakTimeAnim = DOTween.Sequence()
+                          .Append(rectTransform.DOLocalMoveY(10f, 0.5f)
+                          .SetRelative(true)
+                          .SetEase(Ease.OutQuad))
+                          .Join(DOTween.To(() => GetComponent<CanvasGroup>().alpha, a => GetComponent<CanvasGroup>().alpha = a, 0, 0.5f))
+                          .SetLoops(-1, LoopType.Yoyo);
     }
 }
